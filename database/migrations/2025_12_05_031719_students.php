1f3377e1bf10589c1d4eea0b1b9c12d3d2f2e2de
jaysonbutawan <159876164+jaysonbutawan@users.noreply.github.com>
2025-12-05 11:33:59 +0800

    Add migrations for students, teachers, and rooms tables
    Introduces new migration files to create 'students', 'teachers', and 'rooms' tables. These tables include relevant fields and constraints for managing student, teacher, and room data in the database.

---
+++ database/migrations/2025_12_05_031719_students.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->string('student_id')->primary();
+            $table->unsignedInteger('course_id')->nullable();
+            $table->string('email')->unique();
+            $table->string('contact_number', 20)->nullable();
+            $table->string('firstname', 100);
+            $table->string('lastname', 100);
+            $table->integer('year')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->string('status', 50)->nullable();
+
+            $table->foreign('course_id')->references('course_id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
